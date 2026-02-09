303afbec2a527dc2578544db092a8265f9108eb3
NeuroGirl <vesus0206@gmail.com>
2026-02-09 21:52:52 +0300

    Data base data saving and updating issue fixed

--- src/create_collection.py
+++ src/create_collection.py
@@ -6,7 +6,8 @@ def Create_collection(text_chunks, docname):
     os.makedirs(app_data_path, exist_ok=True)
     client = chromadb.PersistentClient(path = app_data_path)
     collection = client.get_or_create_collection(name=docname)
-    collection.add(
+    collection.upsert(
     documents=text_chunks,
     ids=[f'{x}' for x in range(len(text_chunks))])
     return collection
+
