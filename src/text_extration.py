1d882015d138028d6355d40ee2ef10de5345c1d0
MkrtchyanKarina <mkrtchyan.karina.06@mail.ru>
2026-02-09 22:20:26 +0300

    Добавлен класс для извлечения текста из файлов

---
+++ src/text_extration.py
@@ -0,0 +1,71 @@
+from pathlib import Path
+import docx2txt
+import PyPDF2
+from nltk import word_tokenize, SnowballStemmer
+from nltk.stem import WordNetLemmatizer
+import re
+
+main_folder_name = 'documents'  # название корневого каталога
+main_folder = Path.joinpath(Path(__file__).parent.parent, main_folder_name)  # полный путь до корневого каталога
+
+
+class TextCollecting:
+
+    def __init__(self, main_folder_path):
+        self.main_folder_path = main_folder_path
+        self.full_paths = []
+        self.texts = {}
+
+        self.get_full_path(self.main_folder_path)
+
+    def get_full_path(self, folder_path):
+        """ Функция для поиска путей всех файлов """
+        for path in Path.iterdir(folder_path):
+            if Path.is_dir(path):
+                return self.get_full_path(path)
+            else:
+                self.full_paths.append(path)
+        return self.collect_texts()
+
+    def clean_and_lem(self, text):
+        # text = ' '.join(clean(text, punct=True).split())  можно использовать методы строк
+        en_lemmatizer = WordNetLemmatizer()
+        ru_stemmer = SnowballStemmer("russian")
+        lemmatized_words = []
+        tokens = word_tokenize(text)
+        for token in tokens:
+            if re.search(r'[а-яёА-ЯЁ]', token):
+                lemmatized_words.append(ru_stemmer.stem(token))
+            else:
+                lemmatized_words.append(en_lemmatizer.lemmatize(token))
+        return ' '.join(lemmatized_words)
+
+
+    def read_text_from_file(self, path: Path):
+        extension = path.suffix
+        relative_path = path.relative_to(self.main_folder_path)
+        file_text = ""
+        if extension in ('.txt', '.csv', '.json', '.xml', '.html', '.md', '.log', '.py'):
+            with open(path, 'r', encoding='utf-8') as file:
+                file_text = file.read()
+
+        elif extension in ('.doc', '.docx'):
+            file_text = docx2txt.process(path)
+
+        elif extension == '.pdf':
+            with open(path, 'rb') as file:
+                reader = PyPDF2.PdfReader(file)
+                for page in reader.pages:
+                    file_text += page.extract_text()
+        else:
+            pass
+
+        self.texts[relative_path] = self.clean_and_lem(file_text)
+
+    def collect_texts(self):
+        for path in self.full_paths:
+            self.read_text_from_file(path)
+        return self.texts
+
+new_obj = TextCollecting(main_folder)
+print(new_obj.texts)
