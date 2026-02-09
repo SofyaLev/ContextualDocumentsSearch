157de9d19ec8bf3f1fea3b35bb885890e5a4d910
NeuroGirl <vesus0206@gmail.com>
2026-02-09 21:45:55 +0300

    Data Base data saving issue started solving

--- src/create_collection.py
+++ src/create_collection.py
@@ -1,7 +1,10 @@
 import chromadb
+import os
 
 def Create_collection(text_chunks, docname):
-    client = chromadb.Client()
+    app_data_path = os.path.join(os.environ.get('LOCALAPPDATA', 'C:\\Temp'), 'ChromaDBDocStorage')
+    os.makedirs(app_data_path, exist_ok=True)
+    client = chromadb.PersistentClient(path = app_data_path)
     collection = client.get_or_create_collection(name=docname)
     collection.add(
     documents=text_chunks,
