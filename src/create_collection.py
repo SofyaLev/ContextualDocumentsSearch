43eb8d1e785d06862cda40f499304acd91d63bd8
MkrtchyanKarina <mkrtchyan.karina.06@mail.ru>
2026-02-10 13:54:11 +0300

    Создан класс, содержащий функции поиска релевантных документов

--- src/create_collection.py
+++ src/create_collection.py
@@ -6,39 +6,42 @@ from find_best_function import find_best_chunk
 from text_extration import TextExtraction
 from chunking import chunk_text_by_sentence
 
-app_data_path = os.path.join(os.environ.get('LOCALAPPDATA', 'C:\\Temp'), 'ChromaDBDocStorage')
-os.makedirs(app_data_path, exist_ok=True)
-client = chromadb.PersistentClient(path=app_data_path)
-request = 'кошка'
+
+class RelevantDocumentsSearch:
+    def __init__(self, current_folder_path, request, chunk_length=400):
+        self.app_data_path = os.path.join(os.environ.get('LOCALAPPDATA', 'C:\\Temp'), 'ChromaDBDocStorage')
+        os.makedirs(self.app_data_path, exist_ok=True)
+        self.client = chromadb.PersistentClient(path=self.app_data_path)
+        self.request = request
+        self.current_folder_path = current_folder_path
+        self.chunk_length = chunk_length
 
 
-def create_collection(text_chunks, relative_path):
-    collection = client.get_or_create_collection(name=str(uuid.uuid4()))
-    collection.upsert(
-        documents = text_chunks,
-        ids = [f'{x}' for x in range(len(text_chunks))],
-        metadatas=[{'path': str(relative_path)} for _ in range(len(text_chunks))]
-    )
-    return collection
+    def create_collection(self, text_chunks, relative_path):
+        collection = self.client.get_or_create_collection(name=str(uuid.uuid4()))
+        collection.upsert(
+            documents = text_chunks,
+            ids = [f'{x}' for x in range(len(text_chunks))],
+            metadatas=[{'path': str(relative_path)} for _ in range(len(text_chunks))]
+        )
+        return collection
 
 
-def create_documents_collections(current_folder):
-    documents_dict = TextExtraction(current_folder).extract() # {'path': 'text', ...}
-    distances = {}
-    for path in documents_dict:
-        text = documents_dict[path]
-        n = 400
-        chunks = chunk_text_by_sentence(text, n)
-        collection = create_collection(chunks, path)
-        best_chunk, best_distance = find_best_chunk(collection, request)
-        distances[path] = best_distance
-    return distances
+    def find_documents(self) -> dict:
+        documents_dict = TextExtraction(self.current_folder_path).extract()  # {'path': 'text', ...}
+        distances = {}
+        for path in documents_dict:
+            text = documents_dict[path]
+            chunks = chunk_text_by_sentence(text, self.chunk_length)
+            collection = self.create_collection(chunks, path)
+            best_chunk, best_distance = find_best_chunk(collection, self.request)
+            distances[path] = best_distance
+        return distances
 
 
 main_folder_name = 'documents'  # название корневого каталога
 main_folder = Path.joinpath(Path(__file__).parent.parent, main_folder_name)  # полный путь до корневого каталога
 
-d = create_documents_collections(main_folder)
-print(d)
-
-
+d = RelevantDocumentsSearch(main_folder, 'друг').find_documents()
+sorted_d = dict(sorted(d.items(), key=lambda x: x[1]))
+print(sorted_d)
